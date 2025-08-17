c105255eb6dcee149f248d5701e914092d679b51
Cassie L <casserolecodes@gmail.com>
2025-08-17 12:21:19 -0400

    Update App component logic to account for quiz results.

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,37 @@ import Results from './components/Results.vue';
 
 const score = ref(0);
 const isQuizComplete = ref(false);
+const wrongAnswers = ref(0);
 
 </script>
 
 <template>
-  <Quiz 
-    v-if="!isQuizComplete" 
-    :score="score"
-    @updateScore="score += $event"
-    @quizComplete="isQuizComplete=true"
-  />
-  <Results 
-    v-else 
-    :score="score"
-  />
+  <div class="container">
+    <Quiz 
+      v-if="!isQuizComplete" 
+      :score="score"
+      @updateScore="score += $event"
+      @updateIncorrect="wrongAnswers += $event"
+      @quizComplete="isQuizComplete=true"
+    />
+    <Results 
+      v-else 
+      :score="score"
+      :incorrect="wrongAnswers"
+      @quizStartOver="isQuizComplete=false; score=0; wrongAnswers=0"
+    />
+  </div>
 </template>
 
 <style scoped>
-
+  .container {
+    /*TODO responsive*/
+    width: 800px;
+    height: 800px;
+    background-color: white;
+    margin-top: 2rem;
+    padding: 2rem;
+    border: 1rem solid #F38C68;
+    border-radius: 1rem;
+  }
 </style>
